0b03f6eaf2dc3a5e014dbe6816cf004954f9e4ce
igorcl2000 <igorcl2000@gmail.com>
2022-11-23 00:14:41 -0300

    Search1 2.0

---
+++ 2_pygeocode/search1.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+from geopy.geocoders import Nominatim
+if __name__ == '__main__':
+    address = 'Belarmino Vilela Junqueira, Ituiutaba, MG'
+    user_agent = 'Search1'
+    location = Nominatim(user_agent=user_agent).geocode(address)
+    print(location.latitude, location.longitude)
