0a4fec26cabbb2a05755af48aff8c304e65905ce
igorcl2000 <igorcl2000@gmail.com>
2022-11-22 23:31:07 -0300

    Hangman Game 1.0

--- 1_intro_to_py/hangman_game.py
+++ 1_intro_to_py/hangman_game.py
@@ -1,9 +1,45 @@
+import random
+
+
 def play():
+    # Chama mensagem de abertura
+    print_opening()
+
+    # Carrega palavra secreta
+    secret_word = load_secret_word()
+
+
+def print_opening():
     print("*********************************")
     print("***Bem vindo ao jogo da Forca!***")
     print("*********************************")
 
-    print("Fim do jogo")
+
+def load_secret_word():
+    # Esta função sorteia uma palavra secreta
+    # a partir de uma lista de palavras
+    # armazenadas em um arquivo externo
+
+    # abre o arquivo em modo de leitura (r)
+    file = open("words.txt", "r")
+
+    # lista para armazenar as palavras
+    words = []
+
+    for line in file:           # Para cada linha do arquivo
+        line = line.strip()     # Remove o \n do final da linha
+        words.append(line)      # Insere a palavra na lista
+
+    #fecha o arquivo
+    file.close()
+
+    # sorteia um número para escolher a palavra secreta
+    number = random.randrange(0, len(words))
+
+    # seleciona a palavra secreta, coloca todos as letras em maiúsculo
+    # e a retorna
+    secret_word = words[number].upper()
+    return secret_word
 
 
 if __name__ == "__main__":
