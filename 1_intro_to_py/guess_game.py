ae83ccb3192c46a99525934952ed496e8d6b3830
igorcl2000 <igorcl2000@gmail.com>
2022-11-22 23:22:18 -0300

    guess game 5.0

--- 1_intro_to_py/guess_game.py
+++ 1_intro_to_py/guess_game.py
@@ -1,52 +1,52 @@
 import random
-from typing import Any, Union
 
-print("\n*********************************")
-print("Bem vindo ao jogo de Adivinhação!")
-print("*********************************\n")
+def play():
+    print("\n*********************************")
+    print("Bem vindo ao jogo de Adivinhação!")
+    print("*********************************\n")
 
-#secret_number = random.randrange(1, 101)
-secret_number = 50
-points = 1000
-tries = 0
+    secret_number = random.randrange(1, 101)
+    points = 1000
+    tries = 0
 
-print("Defina o nível de dificuldade")
-level = int(input("(1)Fácil (2)Médio (3)Difícil\n"))
+    print("Defina o nível de dificuldade")
+    level = int(input("(1)Fácil (2)Médio (3)Difícil\n"))
 
-if level == 1:
-    tries = 20
-elif level == 2:
-    tries = 10
-else:
-    tries = 5
-
-for run in range(tries):
-    print("\nTentativa {} de {}".format(run+1, tries))
-    guess_str = input("Qual o seu chute? (Entre 1 e 100): ")
-    guess = int(guess_str)
-
-    correct = guess == secret_number
-    bigger = guess > secret_number
-    smaller = guess < secret_number
-
-    if guess < 1 or guess > 100:
-        print("Você deve digitar um número entre 1 e 100!")
-        tries = tries - 1
-        points = points - 100
-        continue
-
-    if correct:
-        print("Acertô miseravi!")
-        bonus = tries * level
-        points = points + bonus
-        break
+    if level == 1:
+        tries = 20
+    elif level == 2:
+        tries = 10
     else:
-        if bigger:
-            print("Errrrooouuu! O seu chute foi maior que o número secreto.")
-        elif smaller:
-            print("Errrrooouuu! O seu chute foi menor que o número secreto.")
+        tries = 5
 
-        points -= abs(secret_number - guess)
+    for run in range(tries):
+        print("\nTentativa {} de {}".format(run+1, tries))
+        guess_str = input("Qual o seu chute? (Entre 1 e 100): ")
+        guess = int(guess_str)
 
-print(f"Pontuação: {points}")
-print("\nFim do Jogo!")
+        correct = guess == secret_number
+        bigger = guess > secret_number
+        smaller = guess < secret_number
+
+        if guess < 1 or guess > 100:
+            print("Você deve digitar um número entre 1 e 100!")
+            points -= 100
+            continue
+
+        if correct:
+            print("Acertô miseravi!")
+            points += (tries - run) * level
+            break
+        else:
+            if bigger:
+                print("Errrrooouuu! O seu chute foi maior que o número secreto.")
+            elif smaller:
+                print("Errrrooouuu! O seu chute foi menor que o número secreto.")
+
+            points -= abs(secret_number - guess)
+
+    print(f"Pontuação: {points}")
+    print("\nFim do Jogo!")
+
+if __name__ == "__main__":
+    play()
