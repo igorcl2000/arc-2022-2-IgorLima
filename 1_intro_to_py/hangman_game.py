c7c36da300aa037d923ba85ecdaeb89108a9dd61
igorcl2000 <igorcl2000@gmail.com>
2022-11-22 23:24:14 -0300

    games 1.0

---
+++ 1_intro_to_py/hangman_game.py
@@ -0,0 +1,10 @@
+def play():
+    print("*********************************")
+    print("***Bem vindo ao jogo da Forca!***")
+    print("*********************************")
+
+    print("Fim do jogo")
+
+
+if __name__ == "__main__":
+    play()
